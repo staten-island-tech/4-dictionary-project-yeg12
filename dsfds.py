4eee5c5da658f64d7a0be567a3413f4f13c7bd55
yeg12 <yeg@nycstudents.net>
2026-03-10 10:27:15 -0400

    Update dsfds.py

--- dsfds.py
+++ dsfds.py
@@ -1,8 +1,17 @@
-item = {
+""" item = {
     "name": "Samsung 55\" 4K UHD TV",
     "price": 429.99,
     "department": "Televisions",
     "description": "55-inch Ultra HD Smart TV with HDR and built-in streaming apps."
-}
+} """
 
-print (item)
+""" print (item)
+ """
+
+stuff = {
+    "name": "insurance 1 life time covering everything ",
+    "price": 89320.09,
+    "department": "Misc",
+    "description": "covers everthing from death to lost in jungle, just buy it."
+}
+print (stuff)
