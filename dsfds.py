f6ee1bb150ddcde76cf3b69cac0ce056dcfa0757
yeg12 <yeg@nycstudents.net>
2026-03-16 10:17:12 -0400

    Update dsfds.py

--- dsfds.py
+++ dsfds.py
@@ -20,14 +20,34 @@ stuff = [
     }
 ]
 
-k = 0
+price_of_stuff = 0
 for index, item in enumerate(stuff):
     print(index, ":", item["name"])
 want = int(input("what u wnat (number)"))
-while A == False:
+user = False
+while user == False:
     if want == 0:
         print(stuff[0])
+        price_of_stuff += 89320.09
+        cont=input("countiune yes,no")
+        if input == "yes":
+            user == False
+        elif cont == "no":
+            user == True
     elif want == 1:
         print(stuff[1])
+        price_of_stuff += 1000000.09
+        cont=input("countiune yes,no")
+        if input == "yes":
+            user == False
+        elif cont == "no":
+            user == True
+
     elif want == 2:
         print(stuff[2]) 
+        price_of_stuff += 32856.09        
+        cont= input("countiune yes,no")
+        if input == "yes":
+            user == False
+        elif cont == "no":
+            user == True
